08aaec9880417e92a767b689af335bf7d29258ca
Joseph Yi <dissonance@gmail.com>
2020-09-12 11:46:26 -0700

    Generated from Spring Initializr

---
+++ src/main/java/io/resiliency/plumeriarubra/PlumeriaRubraApplication.java
@@ -0,0 +1,13 @@
+package io.resiliency.plumeriarubra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlumeriaRubraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlumeriaRubraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/resiliency/plumeriarubra/PlumeriaRubraApplicationTests.java
@@ -0,0 +1,13 @@
+package io.resiliency.plumeriarubra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlumeriaRubraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
